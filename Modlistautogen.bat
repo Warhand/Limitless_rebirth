72cf1ffee92d872438613772ffd69909a602472f
Warhand <creeper620@gmail.com>
2022-03-24 16:37:32 -0400

    Update Modlistautogen.bat

--- Modlistautogen.bat
+++ Modlistautogen.bat
@@ -1,12 +1,7 @@
-@echo off
 cd "C:\Users\Kylie\curseforge\minecraft\Instances\Limitless Rebirth\mods"
-:start
 tree /f /a > "C:\Users\Kylie\curseforge\minecraft\Instances\Limitless Rebirth\Automodlist.txt"
 echo "file generated"
 date /t
 time /t
 date /t >> "C:\Users\Kylie\curseforge\minecraft\Instances\Limitless Rebirth\Automodlist.txt"
 time /t >> "C:\Users\Kylie\curseforge\minecraft\Instances\Limitless Rebirth\Automodlist.txt"
-Timeout /T 60 /NOBREAK
-cls
-goto start
